874e1ea3ad580f9bba8248d6d0edf1ef8c0c59a3
himeshdevop <himeshdevelopment@gmail.com>
2024-07-23 08:27:17 +0530

    Updated footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer() {
     return (
         <footer className="main-footer">
             <div className="footer-left">
-                Copyright &copy; 2023 <div className="bullet"></div> Re-created
-                By
-                <a href="https://github.com/fhmiibrhimdev"> Fahmi Ibrahim</a>
+                Copyright &copy; 2024 <div className="bullet"></div> Re-created
+                By <a href="https://www.instagram.com/cc_programming_club/">Programming Club Of Curtin Colombo</a>
+                
             </div>
             <div className="footer-right">0.1.0</div>
         </footer>
